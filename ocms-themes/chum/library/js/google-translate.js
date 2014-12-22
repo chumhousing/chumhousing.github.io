9907d58f963c6f0d0aef795098f3229b8349c85b
Landry Horimbere <landryh91@gmail.com>
2014-12-22 13:56:04 -0800

    init

---
+++ ocms-themes/chum/library/js/google-translate.js
@@ -0,0 +1,3 @@
+function googleTranslateElementInit() {
+			  new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.SIMPLE, autoDisplay: false}, 'google_translate_element');
+			}
